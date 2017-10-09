faa60a66b52a1ba99abda7cc371e52854b02b1ac
Alessandro Loriga <alessandro.loriga@florencetech.com>
2017-10-09 09:52:31 +0200

    Moved process input computation to tensorflow Draft for build_model

--- model.py
+++ model.py
@@ -23,59 +23,36 @@ class VolterraModel(object):
         VolterraModel.check_model_parameters(**kwargs)
         self.model_order = kwargs.get('order', 1)
         self.model_memory = kwargs.get('memory', 1)
-        self.inputs = {}
+        # self.inputs = {}
         self.kernels = {}
         self._init_data_structs()
-        self.feed_inputs = {}
+        self.build_model()
 
     def _init_data_structs(self):
-        for order in range(self.model_order):
-            # compute the dimension of the i-th order of the kernel and input
-            # like: first order vector has M coefficients, the second order vector M(M + 1)/2 coefficients and the
-            # third order vector M (M + 1)(M + 2)/6 coefficients
-            dimension = np.prod([(self.model_memory + o) for o in range(order + 1)]) / factorial(order + 1).item()
-            # dimension = np.ceil(dimension)
-            self.inputs[order] = tf.placeholder(tf.float32, [dimension], name="input_order_{}".format(order + 1))
-            self.kernels[order] = tf.placeholder(tf.float32, [dimension], name="kernel_order_{}".format(order + 1))
-            print("Kernel dimension for order {} is {}".format(order, dimension))
+        self.inputs = tf.placeholder(tf.float32, [self.model_memory], name="input")
+        for order in range(1, self.model_order + 1):
+            # init kernels, first order M, second M^2, ... M^N, where M is model memory and N is model order
+            dimension = [self.model_memory for _ in range(order)]
+            self.kernels[order] = tf.get_variable("kernel_order_{}".format(order), shape=dimension, dtype=tf.float32, initializer=tf.random_normal_initializer())
         print("Initialized data structure for the model")
 
-    def set_training_input(self, data_x):
-        """
-        This method must be called before train the model, prepare the input for the various order
-        :param data_x: list , it represents the input signal
-        :return:
-        """
-        last_processed_index = 0
-        self.feed_inputs[1] = []
-        # input for first order
-        while self.model_memory + last_processed_index < len(data_x):
-            slice_end = self.model_memory + last_processed_index
-            self.feed_inputs[1].append(np.float32(data_x[last_processed_index:slice_end]))
-            last_processed_index += 1
-
+    def build_model(self):
+        last_input = self.inputs
         for order in range(2, self.model_order + 1):
-            self.feed_inputs[order] = []
+            order_input = tf.matmul(tf.reshape(self.inputs, [-1, 1]), tf.reshape(last_input, [1, -1]))
+            order_input = tf.reshape(order_input, [self.model_memory**order])
+            last_input = order_input
+            # TODO: to be finished input computing for i-th order and convolutions with kernels
+            print(tf.size(order_input))
 
-        for order in range(2, self.model_order + 1):
-            for x_init, x in zip(self.feed_inputs[1], self.feed_inputs[order - 1]):
-                matrix = np.triu(x_init.reshape(-1, 1) * x) # extract the triangular upper part of matrix returned by x^T * x
-                x_order = matrix.ravel()[np.flatnonzero(matrix)]
-                self.feed_inputs[order].append(x_order)
+    def train(self, train_x, train_y):
+        pass
 
-        # TODO there is a bug after the second order, dimensions don't match
-        print(np.size(self.feed_inputs[1][0]))
-        print(np.size(self.feed_inputs[2][0]))
-        print(np.size(self.feed_inputs[3][0]))
+    def test(self, test_x, test_y):
+        pass
 
-        def train(train_x, train_y):
-            pass
-
-        def test(test_x, test_y):
-            pass
-
-        def predict(x):
-            pass
+    def predict(self, x):
+        pass
 
 
 
--- tests.py
+++ tests.py
@@ -5,6 +5,5 @@ __author__ = 'aloriga'
 
 def init_test():
     model = VolterraModel(order=3, memory=3)
-    model.set_training_input([1, 1, 2, 3, 5, 8, 13, 21])
 
 init_test()
